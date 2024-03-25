4f84200db41b18a6285acc59e20425af212d5faf
MAHIR HAYAAT <mahirhayat4u@gmail.com>
2024-03-25 14:27:31 +0530

    sidebar link

--- StudyNotion/src/components/core/Dashboard/Sidebar.jsx
+++ StudyNotion/src/components/core/Dashboard/Sidebar.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
-
+import {useSelector} from "react-redux"
 import {sidebarLinks} from "../../../data/dashboard-links"
 import { logout } from '../../../services/operations/authAPI'
+import Spinner from '../../common/Spinner'
+import SidebarLink from './SidebarLink'
 
 const Sidebar = () => {
 
+   const {user ,loading:profileLoading}= useSelector( (state)=>state.auth )     
+   const { loading:authLoading}= useSelector( (state)=>state.auth )     
 
+   if (authLoading || profileLoading) {
+    return (
+        <div className='mt-12'>
+          <Spinner/>
+        </div>
+        
+    )
+}
    
   return (
     <div> 
-    
+        <div className='flex flex-col min-w-[250px] border-r-[1px] border-r-richblack-700 h-[calc(100vh-3.5rem)] bg-richblack-700 py-10'>
+
+          <div className='flex flex-col'>
+
+             {
+              sidebarLinks.map((link,index)=>{
+                if(link.type && user?.accountType !== link.type) return null;
+                return (
+                  <SidebarLink />
+                )
+              })
+             }
+
+          </div>
+
+        </div>
     </div>
   )
 }
---
+++ StudyNotion/src/components/core/Dashboard/SidebarLink.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import * as Icons from "react-icons/vsc"
+import { useDispatch } from 'react-redux'
+import {useLocation} from "react-router-dom"
+const SidebarLink = ({link,iconName}) => {
+
+    const Icon=Icons[iconName]
+    const location =useLocation();
+    const dispatch=useDispatch();
+
+    const matchRoute=(route)=>{
+        return matchRoute({path:route},location.pathname)
+    }
+
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default SidebarLink
