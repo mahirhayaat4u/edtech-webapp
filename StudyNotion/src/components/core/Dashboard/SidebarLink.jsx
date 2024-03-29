8cd1d47b2827530e89a4e5f05e627d2849f452ed
MAHIR HAYAAT <mahirhayat4u@gmail.com>
2024-03-29 23:00:12 +0530

    sidebar links

--- StudyNotion/src/components/core/Dashboard/SidebarLink.jsx
+++ StudyNotion/src/components/core/Dashboard/SidebarLink.jsx
@@ -13,9 +13,29 @@ const SidebarLink = ({link,iconName}) => {
     }
 
   return (
-    <div>
-    
-    </div>
+    <NavLink
+    to={link.path}
+    className={`relative px-8 py-2 text-sm font-medium ${
+        matchRoute(link.path)
+          ? "bg-yellow-800 text-yellow-50"
+          : "bg-opacity-0 text-richblack-300"
+      } transition-all duration-200`}
+    >
+    {/* span is for yellow border */}
+    <span
+        className={`absolute left-0 top-0 h-full w-[0.15rem] bg-yellow-50 ${
+          matchRoute(link.path) ? "opacity-100" : "opacity-0"
+        }`}
+      ></span>
+
+      <div className="flex items-center gap-x-2">
+      
+            {/* Icon Goes Here */}
+            <Icon className="text-lg" />
+            <span>{link.name}</span>
+      </div>
+          
+    </NavLink>
   )
 }
 
